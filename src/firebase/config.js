cb64d6000e67d7201fee371b7e982f9a96dcce48
zawyehtoo <zawyehtoo29@gmail.com>
2023-11-03 16:59:31 +0630

    clean setup

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app'
+const firebaseConfig = {
+    apiKey: "AIzaSyAYUl426R8wd58pxETnsmRsOKhwQDInryg",
+    authDomain: "live-chat-1096d.firebaseapp.com",
+    projectId: "live-chat-1096d",
+    storageBucket: "live-chat-1096d.appspot.com",
+    messagingSenderId: "841503491359",
+    appId: "1:841503491359:web:ddadfbde9051637451dc2a"
+  };
